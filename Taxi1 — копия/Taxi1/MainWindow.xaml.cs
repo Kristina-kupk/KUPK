5e6711939fcab66b89e2687529e99c76e2a75b2f
Kristina-kupk <55569499+Kristina-kupk@users.noreply.github.com>
2020-05-20 10:58:10 +0500

    Add files via upload

---
+++ Taxi1 — копия/Taxi1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Taxi1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Klient_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 mw = new Window1();
+            mw.Show();
+            this.Hide();
+            Close();
+        }
+
+        private void Voditeli_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 mw = new Window2();
+            mw.Show();
+            this.Hide();
+            Close();
+        }
+    }
+}
